cabca2f2e89a2f794868fda951f4fe153afd0167
singhprince1625 <singhprince73700@gmail.com>
2023-11-26 23:08:58 +0530

    1st commit on 26/11/2023

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
+class Product{
+
+    public int product(int x, int y) {
+        return x+y;
+    }
+
+    public int product(int x, int y, int z) {
+        return x+y+z;
+    }
+
+    public double product(double x, double y) {
+        return x+y;
+    }
+}
+
 public class Main {
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(5,7));
+        System.out.println(p.product(5 , 6 , 7));
+        System.out.println(p.product(5.5, 2.3));
+    }
+
+
+
 
 }
